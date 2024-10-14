b0e4e7483cdbb63e7aa16b60fa7334e718cb5d6f
ishaan <ishaan.mohan.sinha@gmail.com>
2024-10-14 11:24:21 -0400

    gale shapley implemented

--- assignment2/match.py
+++ assignment2/match.py
@@ -1,3 +1,5 @@
+import random
+
 import numpy as np
 from typing import List, Tuple
 
@@ -21,7 +23,64 @@ def run_matching(scores: List[List], gender_id: List, gender_pref: List) -> List
             - What data structure can you use to take advantage of this fact when forming your matches?
         - This is by no means an exhaustive list, feel free to reach out to us for more help!
     """
-    matches = [()]
+
+    N = len(scores)
+
+
+    def is_compatible(proposer_id, receiver_id):
+        proposer_gender = gender_id[proposer_id]
+        receiver_gender = gender_id[receiver_id]
+        proposer_pref = gender_pref[proposer_id]
+        receiver_pref = gender_pref[receiver_id]
+
+        if proposer_gender=="Male":
+            if proposer_pref=="Women" and receiver_gender=="Female" and receiver_pref=="Men":
+                return True
+            if proposer_pref=="Men" and receiver_gender=="Male" and receiver_pref!="Women":
+                return True
+            if proposer_pref=="Bisexual" and (receiver_pref=="Bisexual" or receiver_pref==proposer_gender):
+                return True
+        elif proposer_gender=="Female":
+            if proposer_pref=="Men" and receiver_gender=="Male" and receiver_pref=="Women":
+                return True
+            if proposer_pref=="Women" and receiver_gender=="Female" and receiver_pref!="Male":
+                return True
+            if proposer_pref=="Bisexual" and (receiver_pref=="Bisexual" or receiver_pref==proposer_gender):
+                return True
+        elif proposer_gender=="Nonbinary":
+            if receiver_pref=="Bisexual":
+                return True
+        return False
+
+    for i in range(N):
+        for j in range(N):
+            if not is_compatible(i, j):
+                scores[i][j] = 0
+
+    proposers = random.sample(range(N), N//2)
+    receivers = list(set(range(N)) - set(proposers))
+
+    proposer_prefs = {p: sorted(range(N), key=lambda x: scores[p][x], reverse=True) for p in proposers}
+    receiver_prefs = {r: sorted(range(N), key=lambda x: scores[x][r], reverse=True) for r in receivers}
+
+    free_proposers = proposers[:]
+    matches = {}
+    proposed = {p: [] for p in proposers}
+
+    while free_proposers:
+        proposer = free_proposers.pop(0)
+        receiver = proposer_prefs[proposer].pop(0)
+        if receiver not in matches:
+            matches[receiver] = proposer
+        else:
+            current_match = matches[receiver]
+            if receiver_prefs[receiver].index(proposer) < receiver_prefs[receiver].index(current_match):
+                matches[receiver] = proposer
+                free_proposers.append(current_match)
+            else:
+                free_proposers.append(proposer)
+
+    matches = [(proposer, receiver) for receiver, proposer in matches.items()]
     return matches
 
 if __name__ == "__main__":
@@ -39,3 +98,4 @@ if __name__ == "__main__":
             gender_preferences.append(curr)
 
     gs_matches = run_matching(raw_scores, genders, gender_preferences)
+    print(gs_matches)
